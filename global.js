34bcde89f2408b7395b29b4c159791b235133425
wwohlers <55663618+wwohlers@users.noreply.github.com>
2020-05-23 16:18:12 -0400

    Update global.js

--- global.js
+++ global.js
@@ -1,4 +1,4 @@
-const prod = false;
+const prod = true;
 
 export const http = prod ? "/api" : "http://localhost:3000";
 export const front = prod ? "https://thehuskyhusky.com" : "http://localhost:8080";
