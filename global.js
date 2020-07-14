5c6609bb3d16ced5f8bd37aeb710683424c0583a
Bill <wohlers.w@husky.neu.edu>
2020-07-14 16:49:46 -0400

    Committed!

--- global.js
+++ global.js
@@ -1,5 +1,5 @@
 // change to true when in production environment
-const prod = true;
+const prod = false;
 
 export const http = prod ? "/api" : "http://localhost:3000";
 export const front = prod ? "https://thehuskyhusky.com" : "http://localhost:8080";
